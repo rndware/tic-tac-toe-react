99350a117f2085f83632a5586aa7024d369862b8
rndware <Adam.Smith@toshibagcs.com>
2024-06-05 13:29:18 +0100

    Add select form control

---
+++ src/components/settings-form/form-controls/SelectFormControl.tsx
@@ -0,0 +1,56 @@
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { SelectChangeEvent } from "@mui/material/Select";
+
+import { I18nCopy } from "../../../types/app";
+
+import styles from "../SettingsForm.module.scss";
+
+interface EnumMap {
+  [key: string]: string;
+}
+
+type Options = { [key: string]: string };
+
+export interface SelectFormControlData {
+  key: string;
+  copy: I18nCopy;
+  value: string;
+  enum: EnumMap;
+  onChange: (e: SelectChangeEvent<string>) => void;
+  options: Options;
+}
+
+const renderSelectOptions = (
+  key: string,
+  enumOptions: EnumMap,
+  options: Options,
+) =>
+  Object.keys(enumOptions).map((value: string) => (
+    <MenuItem key={`${key}-options-item-${value}`} value={value}>
+      {options[value.toString().toLowerCase()]}
+    </MenuItem>
+  ));
+
+export const SelectFormControl = (item: SelectFormControlData) => (
+  <div
+    key={`form-control-item-${item.key}`}
+    className={styles.SettingsForm__formControl}
+  >
+    <FormControl>
+      <InputLabel id={`${item.key}-label`}>{item.copy.label}</InputLabel>
+      <Select
+        className={styles.Select}
+        labelId={`${item.key}-label`}
+        id={`${item.key}-select`}
+        value={item.value}
+        label={item.copy.label}
+        onChange={item.onChange}
+      >
+        {renderSelectOptions(item.key, item.enum, item.options)}
+      </Select>
+    </FormControl>
+  </div>
+);
