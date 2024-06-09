f8b4f3751ec64f3c8dba5c4e1f6ecf5f4f0adbfe
rndware <Adam.Smith@toshibagcs.com>
2024-06-09 20:31:29 +0100

    Add missing file

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -2,14 +2,12 @@ import React from "react";
 
 import Typography from "@mui/material/Typography";
 import { useTranslation } from "react-i18next";
-import StarBackground from "../../components/star-background";
 
 const NotFound = () => {
   const { t } = useTranslation();
 
   return (
     <div className="NotFoundPage">
-      <StarBackground />
       <Typography variant="h3" component="h1">
         {t("notFoundPage.description")}
       </Typography>
